ef75fafe15f75f2943cdcdd0a899864c2acb784d
Frankacer <frankbright211@gmail.com>
2023-11-01 19:27:00 -0700

    print string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def islower(c):
+    return ord('a') <= ord(c) <= ord('z')
+
+
+def uppercase(str):
+    offset = ord('a') - ord('A')
+    for i in range(len(str)):
+        if islower(str[i]):
+            c = str[i] - offset
+        else:
+            c = str[i]
+        print("{}".format(c))
