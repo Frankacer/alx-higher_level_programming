9068687bfe423c8e3117e3ea6eb6a3f7319d2d26
Frankacer <frankbright211@gmail.com>
2023-11-01 20:20:19 -0700

    pow function and fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            item = "Fizz"
+        elif i % 5 == 0:
+            item = "Buzz"
+        elif i % 3 == 0 and i % 5 == 0:
+            item = "FizzBuzz"
+        else:
+            item = i
+        print(item, end=" ")
+    print("\n", end="")
