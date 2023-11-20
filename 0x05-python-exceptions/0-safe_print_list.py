d08b453965b65db3f60821eddd6df0fc9808cd7f
Frankacer <frankbright211@gmail.com>
2023-11-20 14:18:50 -0800

    Handled IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         for e in my_list:
-            if e == x:
+            if i == x:
                 break
             print("{}".format(e), end="")
             i += 1
