8f87a96afba1fc5d8f75fe1f2cf438ea6a1faf0e
Frankacer <frankbright211@gmail.com>
2023-11-21 15:42:05 -0800

    Create square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class to represent a square object.
+"""
+
+
+class Square:
+    """
+    Defines a square
+    """
+    def __init__(self, size):
+        """
+        Creates a square of size <__size>
+        
+        Args:
+        size (int): size of square
+        """
+        self.__size = size
